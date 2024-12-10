0098a7468596dcf9c3b9cc9e9b794e4e9a055603
Brian McSwiggen <brian.mcswiggen@gmail.com>
2024-12-10 16:55:23 -0500

    Update FileReader

--- app/src/main/kotlin/common/FileReader.kt
+++ app/src/main/kotlin/common/FileReader.kt
@@ -2,10 +2,25 @@ package common
 
 class FileReader {
 
+    /**
+     * Deprecated, prefer [readFileLines]
+     */
+    @Deprecated("Prefer readFileLines, which is more clear")
     fun readFile(filename: String): List<String> {
         val input = this::class.java.getResourceAsStream("/$filename")
         val lines = input?.bufferedReader()?.readLines()
 
         return lines ?: emptyList()
+    }
+
+    fun readFileLines(filename: String): List<String> {
+        return readFile(filename)
+    }
+
+    fun readFileText(filename: String): String {
+        val input = this::class.java.getResourceAsStream("/$filename")
+        val lines = input?.bufferedReader()?.readText()
+
+        return lines ?: ""
     }
 }
--- app/src/test/kotlin/common/FileReaderTest.kt
+++ app/src/test/kotlin/common/FileReaderTest.kt
@@ -7,16 +7,25 @@ class FileReaderTest {
     @Test fun loadsFileLines() {
         val classUnderTest = FileReader()
 
-        val actual = classUnderTest.readFile("test.txt")
+        val actual = classUnderTest.readFileLines("test.txt")
 
         val expected = listOf("line1", "line2", "line3")
+        assertEquals(expected, actual)
+    }
+
+    @Test fun loadsFileText() {
+        val classUnderTest = FileReader()
+
+        val actual = classUnderTest.readFileText("test.txt")
+
+        val expected = "line1\nline2\nline3"
         assertEquals(expected, actual)
     }
 
     @Test fun noFile() {
         val classUnderTest = FileReader()
 
-        val actual = classUnderTest.readFile("doesnotexist.txt")
+        val actual = classUnderTest.readFileLines("doesnotexist.txt")
 
         assertEquals(emptyList(), actual)
     }
