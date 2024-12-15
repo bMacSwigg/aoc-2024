3e49e98033de68d0a3b531f3ee0f5683964ac104
Brian McSwiggen <brian.mcswiggen@gmail.com>
2024-12-15 10:48:44 -0500

    Update Point.kt

--- app/src/main/kotlin/common/Point.kt
+++ app/src/main/kotlin/common/Point.kt
@@ -1,6 +1,14 @@
 package common
 
 data class Point(val x: Int, val y: Int) {
+
+    companion object {
+        val UP: Point = Point(0, -1)
+        val RIGHT: Point = Point(1, 0)
+        val DOWN: Point = Point(0, 1)
+        val LEFT: Point = Point(-1, 0)
+    }
+
     operator fun plus(other: Point): Point {
         return Point(x + other.x, y + other.y)
     }
