f9326544e6989acc34181445d3ff440687a6c1dc
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-01-16 12:50:19 -0500

    Day 21 part 2 solution

--- app/src/main/kotlin/day21/Day21.kt
+++ app/src/main/kotlin/day21/Day21.kt
@@ -7,20 +7,9 @@ import common.*
 import java.lang.StringBuilder
 
 
-class KeypadTraverser(var pos: Point) {
-
-    fun costTo(other: Point): Int {
-        return -1
-    }
-
-    fun moveTo(other: Point) {
-        pos = other
-    }
-}
-
-data class KeypadNode(override val loc: Point, val prev: Char, val cost: (s: Char, t: Char) -> Int): Node(loc) {
-    override fun reachable(): Map<Node, Int> {
-        val nodes: Map<Node, Int> = mapOf(
+data class KeypadNode(override val loc: Point, val prev: Char, val cost: (s: Char, t: Char) -> Long): Node(loc) {
+    override fun reachable(): Map<Node, Long> {
+        val nodes: Map<Node, Long> = mapOf(
             Pair(KeypadNode(loc+Point.UP, '^', cost), cost(prev, '^')),
             Pair(KeypadNode(loc+Point.LEFT, '<', cost), cost(prev, '<')),
             Pair(KeypadNode(loc+Point.DOWN, 'v', cost), cost(prev, 'v')),
@@ -28,14 +17,17 @@ data class KeypadNode(override val loc: Point, val prev: Char, val cost: (s: Cha
             Pair(KeypadNode(loc, 'A', cost), cost(prev, 'A')))
         return nodes
     }
+}
 
+data class CacheKey(val layers: Int, val p1: Point, val c1: Char, val p2: Point, val c2: Char) {
+    constructor(layers: Int, n1: KeypadNode, n2: KeypadNode): this(layers, n1.loc, n1.prev, n2.loc, n2.prev)
 }
 
 class InputReader(fileReader: FileReader, filename: String) {
 
     private val codes = fileReader.readFileLines(filename)
-    val numericKeypad: ImmutableBiMap<Char, Point>
-    val directionalKeypad: ImmutableBiMap<Char, Point> = ImmutableBiMap.of(
+    private val numericKeypad: ImmutableBiMap<Char, Point>
+    private val directionalKeypad: ImmutableBiMap<Char, Point> = ImmutableBiMap.of(
         '^', Point(1, 0),
         'A', Point(2, 0),
         '<', Point(0, 1),
@@ -59,138 +51,69 @@ class InputReader(fileReader: FileReader, filename: String) {
         numericKeypad = numericKeypadBuilder.build()
     }
 
-    val directionalTiles = arrayOf(
+    private val directionalTiles = arrayOf(
         charArrayOf('#', '.', '.'),
         charArrayOf('.', '.', '.')
     )
-    var numericTiles = arrayOf(
+    private var numericTiles = arrayOf(
         charArrayOf('.', '.', '.'),
         charArrayOf('.', '.', '.'),
         charArrayOf('.', '.', '.'),
         charArrayOf('#', '.', '.')
     )
 
-//    fun costOf(code: String, keypad: Map<Char, Point>, cost: (s: Char, t: Char) -> Int, tiles: Array<CharArray>): Int {
-//        var sum = 0
-//        var start = KeypadNode(keypad['A']!!, 'A', cost)
-//        for (c in code) {
-//            val end = KeypadNode(keypad[c]!!, cost)
-//            sum += Dijkstra(Board(tiles, start, end)).run()
-//            sum += costs['A']!!
-//            start = end
-//        }
-//        return sum
-//    }
+    private val costCache: MutableMap<CacheKey, Long> = mutableMapOf()
 
-    fun costOfNumeric(code: String): Int {
-        var sum = 0
+    private fun costOfNumeric(layers: Int, code: String): Long {
+        var sum = 0L
         var start = 'A'
         for (c in code) {
-            sum += costOnNumericKeypad(start, c)
+            sum += costOnNumericKeypad(layers, start, c)
             start = c
-
         }
         return sum
     }
 
-    fun costOnNumericKeypad(s: Char, t: Char): Int {
-        val start = KeypadNode(numericKeypad[s]!!, 'A', this::costOnRadioactiveKeypad)
-        val end = KeypadNode(numericKeypad[t]!!, 'A', this::costOnRadioactiveKeypad)
+    private fun costOnNumericKeypad(layers: Int, s: Char, t: Char): Long {
+        val costFn = this.costFnOnDirectionalKeypad(layers)
+        val start = KeypadNode(numericKeypad[s]!!, 'A', costFn)
+        val end = KeypadNode(numericKeypad[t]!!, 'A', costFn)
         val dijkstra = Dijkstra(Board(numericTiles, start, end))
         val cost = dijkstra.run(1)
-//        println("$s -> $t: $cost")
         return cost
     }
 
-    private fun costFnOnDirectionalKeypad(layers: Int): (Char, Char) -> Int {
+    private fun costFnOnDirectionalKeypad(layers: Int): (Char, Char) -> Long {
         val costFn = if (layers <= 2) {
             this::costOnColdKeypad
         } else {
             this.costFnOnDirectionalKeypad(layers-1)
         }
-        return { s: Char, t: Char ->
+        return fun(s: Char, t: Char): Long {
             val start = KeypadNode(directionalKeypad[s]!!, 'A', costFn)
             val end = KeypadNode(directionalKeypad[t]!!, 'A', costFn)
-            val dijkstra = Dijkstra(Board(directionalTiles, start, end))
-            dijkstra.run(1)
-        }
 
-    }
-
-    fun costOnRadioactiveKeypad(s: Char, t: Char): Int {
-        val start = KeypadNode(directionalKeypad[s]!!, 'A', this::costOnColdKeypad)
-        val end = KeypadNode(directionalKeypad[t]!!, 'A', this::costOnColdKeypad)
-        val dijkstra = Dijkstra(Board(directionalTiles, start, end))
-        val cost = dijkstra.run(1)
-        if (dijkstra.endNode == null) {
-            println("$s, $t, $cost")
-        }
-        return cost
-    }
-
-    fun costOnColdKeypad(s: Char, t: Char): Int {
-        return (directionalKeypad[s]!!.gridDistance(directionalKeypad[t]!!)) + 1
-    }
-
-    private fun pathBetween(start: Point, end: Point): String {
-        val sb = StringBuilder()
-        val diff = end - start
-        // As long as you always go right before going left, you won't hit the empty spot on either keypad
-        if (diff.x > 0) {
-            sb.append(">".repeat(diff.x))
-        }
-        if (diff.y > 0) {
-            sb.append("v".repeat(diff.y))
-        }
-        if (diff.y < 0) {
-            sb.append("^".repeat(-diff.y))
-        }
-        if (diff.x < 0) {
-            sb.append("<".repeat(-diff.x))
-        }
-        sb.append('A')
-        return sb.toString()
-    }
-
-    fun instructionsForCode(code: String, keypad: Map<Char, Point>): String {
-        val sb = StringBuilder()
-        var start = keypad['A']!!
-        for (c in code) {
-            val end = keypad[c]!!
-            // println("$c: $start -> $end (${end - start})")
-            sb.append(pathBetween(start, end))
-            start = end
-        }
-        return sb.toString()
-    }
-
-    fun fullInstructionsForCode(code: String): String {
-        val iter1 = instructionsForCode(code, numericKeypad)
-        val iter2 = instructionsForCode(iter1, directionalKeypad)
-        val iter3 = instructionsForCode(iter2, directionalKeypad)
-        return iter3
-    }
-
-    fun simulate(code: String, keypad: BiMap<Char, Point>): String {
-        var p: Point = keypad['A']!!
-        val sb = StringBuilder()
-        for (c in code) {
-            when (c) {
-                '>' -> p += Point.RIGHT
-                '<' -> p += Point.LEFT
-                '^' -> p += Point.UP
-                'v' -> p += Point.DOWN
-                'A' -> sb.append(keypad.inverse()[p]!!)
+            val cacheKey = CacheKey(layers, start, end)
+            if (costCache.containsKey(cacheKey)) {
+                return costCache[cacheKey]!!
             }
+
+            val dijkstra = Dijkstra(Board(directionalTiles, start, end))
+            val cost = dijkstra.run(1)
+            costCache[cacheKey] = cost
+            return cost
         }
-        return sb.toString()
+
     }
 
-    fun codesComplexity(): Int {
-        var sum = 0
+    fun costOnColdKeypad(s: Char, t: Char): Long {
+        return (directionalKeypad[s]!!.gridDistance(directionalKeypad[t]!!)) + 1L
+    }
+
+    fun codesComplexity(layers: Int): Long {
+        var sum = 0L
         for (code in codes) {
-//            val instr = fullInstructionsForCode(code)
-            val cost = costOfNumeric(code)
+            val cost = costOfNumeric(layers, code)
             val numeric = code.substring(0, code.length-1).toInt()
             println("$code: $cost, $numeric")
             sum += cost * numeric
@@ -202,52 +125,10 @@ class InputReader(fileReader: FileReader, filename: String) {
 fun main() {
     val sw = Stopwatch.createStarted()
     val inputReader = InputReader(FileReader(), "day21.txt")
-    println(inputReader.codesComplexity())
-//    println(inputReader.instructionsForCode("029A", inputReader.numericKeypad))
-//    println(inputReader.instructionsForCode("029A", inputReader.numericKeypad).length)
-//    println(inputReader.costOnColdKeypad('A', 'v')) // 3
-//    println(inputReader.costOnColdKeypad('v', '<')) // 2
-//    println(inputReader.costOnColdKeypad('<', '<')) // 1
-//    println(inputReader.costOnColdKeypad('<', 'A')) // 4
-//    println(inputReader.costOnRadioactiveKeypad('A', '<')) // 10
-//    println(inputReader.costOfNumeric("029A"))
-//    testCosts(">^^A", inputReader::costOnRadioactiveKeypad)
-//    testCosts("vA<^AA>A", inputReader::costOnColdKeypad)
-//    println(inputReader.costOfNumeric("980A"))
-//    println(inputReader.costOfNumeric("179A"))
-//    println(inputReader.costOfNumeric("456A"))
-//    println(inputReader.fullInstructionsForCode("379A"))
-//    println(inputReader.costOfNumeric("379A"))
-//    println(inputReader.simulate("v<<A>>^AvA^A", inputReader.directionalKeypad))
-//    println(inputReader.simulate("<A>A", inputReader.directionalKeypad))
-//    println(inputReader.simulate("^A", inputReader.numericKeypad))
-//    println(inputReader.simulate("v<<A>>^AAv<A<A>>^AAvAA^<A>A", inputReader.directionalKeypad))
-//    println(inputReader.simulate("<AAv<AA>>^A", inputReader.directionalKeypad))
-//    println(inputReader.simulate("^A^^<<A", inputReader.numericKeypad))
-//    println(inputReader.simulate("<vA<AA>>^AAvA<^A>AAvA^A", inputReader.directionalKeypad))
-//    println(inputReader.simulate("v<<AA>^AA>A", inputReader.directionalKeypad))
-//    println(inputReader.simulate("^A<<^^A", inputReader.numericKeypad))
-//    println(inputReader.costOnNumericKeypad('3', '7'))
-//    println(inputReader.costOfNumeric("37"))
-//    var sum = 0
-//    var s = 'A'
-//    for (c in "<<^^A") {
-//        val cost = inputReader.costOnRadioactiveKeypad(s, c)
-//        println("$s -> $c: $cost")
-//        s = c
-//        sum += cost
-//    }
-//    println("$sum")
-//
-//    sum = 0
-//    s = 'A'
-//    for (c in "^^<<A") {
-//        val cost = inputReader.costOnRadioactiveKeypad(s, c)
-//        println("$s -> $c: $cost")
-//        s = c
-//        sum += cost
-//    }
-//    println("$sum")
+    println(inputReader.codesComplexity(2))
+    println(sw.elapsed())
+    println(inputReader.codesComplexity(25))
+    println(sw.stop().elapsed())
 }
 
 fun testCosts(str: String, costFn: (s: Char, t: Char) -> Int) {
@@ -261,21 +142,3 @@ fun testCosts(str: String, costFn: (s: Char, t: Char) -> Int) {
     }
     println("$sum")
 }
-
-// v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A
-// <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-
-// <A>A<AAv<AA>>^AvAA^Av<AAA>^A
-// 4422441324121323312232411232
-//       *   * *
-
-/*
- * <vA<AA>>^AvAA<^A>A <v<A>>^AvA^A <vA>^A<v<A>^A>AAvA^A <v<A>A>^AAAvA<^A>A
- * v<<A>>^A <A>A vA<^AA>A <vAAA>^A
- * <A ^A >^^A vvvA
- * 029A
- */
-// <vA<AA>>^AvAA<^A>A = A -> 0 (18)
-// <v<A>>^AvA^A = 0 -> 2 (12)
-// <vA>^A<v<A>^A>AAvA^A = 2 -> 9 (20!)
-// <v<A>A>^AAAvA<^A>A = 9 -> A (18!)
