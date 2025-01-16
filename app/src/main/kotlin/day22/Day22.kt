a773a04ee24af0edcfae1589ec4c6dcac742c9fa
Brian McSwiggen <brian.mcswiggen@gmail.com>
2025-01-16 15:48:27 -0500

    Day 22 part 2 solution

--- app/src/main/kotlin/day22/Day22.kt
+++ app/src/main/kotlin/day22/Day22.kt
@@ -26,6 +26,64 @@ fun prune(a: Long): Long {
     return a.mod(16777216L)
 }
 
+data class Sequence(val a: Int, val b: Int, val c: Int, val d: Int) {
+
+    fun shift(next: Int): Sequence {
+        return Sequence(b, c, d, next)
+    }
+}
+
+class Monkey(val initial: Long) {
+    val pricesBySeq: Map<Sequence, Int>
+
+    init {
+        val mapBuilder = mutableMapOf<Sequence, Int>()
+
+        val sec1 = generateNext(initial)
+        val sec2 = generateNext(sec1)
+        val sec3 = generateNext(sec2)
+        val sec4 = generateNext(sec3)
+        val a = sec1.mod(10)
+        val b = sec2.mod(10)
+        val c = sec3.mod(10)
+        val d = sec4.mod(10)
+        var sequence = Sequence(a - initial.mod(10), b - a, c - b, d - c)
+        mapBuilder[sequence] = d
+
+        var prev = sec4
+        var prevPrice = d
+        for (i in 5..2000) {
+            val next = generateNext(prev)
+            val price = next.mod(10)
+            val diff = price - prevPrice
+            sequence = sequence.shift(diff)
+            if (!mapBuilder.containsKey(sequence)) {
+                // only insert if it's new: we can only sell at the _first_ occurence of a sequence
+                mapBuilder[sequence] = price
+            }
+            prev = next
+            prevPrice = price
+        }
+
+        pricesBySeq = mapBuilder
+    }
+}
+
+class MonkeyMarket() {
+    val totalPriceBySequence = mutableMapOf<Sequence, Int>()
+
+    fun add(monkey: Monkey) {
+        for (entry in monkey.pricesBySeq) {
+            val totalSoFar = totalPriceBySequence.getOrDefault(entry.key, 0)
+            totalPriceBySequence[entry.key] = totalSoFar + entry.value
+        }
+    }
+
+    fun max(): Int {
+        return totalPriceBySequence.maxOf { it.value }
+    }
+}
+
 class InputReader(fileReader: FileReader, filename: String) {
     private val initialValues = fileReader.readFileLines(filename).map { it.toLong() }
 
@@ -40,11 +98,18 @@ class InputReader(fileReader: FileReader, filename: String) {
         }
         return sum
     }
+
+    fun findMax(): Int {
+        val mm = MonkeyMarket()
+        initialValues.map { Monkey(it) }.forEach { mm.add(it) }
+        return mm.max()
+    }
 }
 
 fun main() {
     val sw = Stopwatch.createStarted()
     val inputReader = InputReader(FileReader(), "day22.txt")
     println(inputReader.sum2000steps())
+    println(inputReader.findMax())
     println(sw.stop().elapsed())
 }
--- app/src/test/kotlin/day22/Day22Test.kt
+++ app/src/test/kotlin/day22/Day22Test.kt
@@ -5,11 +5,19 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day22Test {
-    @Test fun codesComplexity() {
+    @Test fun sum2000steps() {
         val classUnderTest = InputReader(FileReader(), "day22-example.txt")
 
         val actual = classUnderTest.sum2000steps()
 
         assertEquals(37327623L, actual)
+    }
+
+    @Test fun findMax() {
+        val classUnderTest = InputReader(FileReader(), "day22-example2.txt")
+
+        val actual = classUnderTest.findMax()
+
+        assertEquals(23, actual)
     }
 }
